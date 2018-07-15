72d1b19b6239b80182a7624b8c239a90d19e65ea
suzuki <fujifabric0522@gmail.com>
2018-07-16 06:15:47 +0900

    修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,19 +3,19 @@ module ApplicationHelper
     twitter_card = {}
     if post.present?
       if post.id.present?
-        twitter_card[:url] = "https://[アプリ名].herokuapp.com/posts/#{post.id}"
-        twitter_card[:image] = "https://s3-ap-northeast-1.amazonaws.com/[アプリ名]-production/images/#{post.id}.png"
+        twitter_card[:url] = "https://answer-me-716.herokuapp.com/posts/#{post.id}"
+        twitter_card[:image] = "https://s3-ap-northeast-1.amazonaws.com/answer-me-716-production/images/#{post.id}.png"
       else
-        twitter_card[:url] = 'https://[アプリ名].herokuapp.com/'
-        twitter_card[:image] = "https://raw.githubusercontent.com/[GitHubのユーザーID]/[アプリ名]/master/app/assets/images/top.png"
+        twitter_card[:url] = 'https://answer-me-716.herokuapp.com/'
+        twitter_card[:image] = "https://raw.githubusercontent.com/wonda12/answer-me-716/master/app/assets/images/top.png"
       end
     else
-      twitter_card[:url] = 'https://[アプリ名].herokuapp.com/'
-      twitter_card[:image] = "https://raw.githubusercontent.com/[GitHubのユーザーID]/[アプリ名]/master/app/assets/images/top.png"
+      twitter_card[:url] = 'https://answer-me-716.herokuapp.com/'
+      twitter_card[:image] = "https://raw.githubusercontent.com/wonda12/answer-me-716/master/app/assets/images/top.png"
     end
-    twitter_card[:title] = "画像下に表示されるタイトル"
+    twitter_card[:title] = "Answer.me"
     twitter_card[:card] = 'summary_large_image'
-    twitter_card[:description] = '画像下に表示される説明文'
+    twitter_card[:description] = '質問に答えてみましょう！'
     twitter_card
   end
 end
