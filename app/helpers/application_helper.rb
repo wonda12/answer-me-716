8130f496d71aa37599760b66a88689db94bb1fee
suzuki <fujifabric0522@gmail.com>
2018-07-29 14:11:31 +0900

    微修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,10 @@
 module ApplicationHelper
+  # application.html.erbから@postかnilを受け取って返す
   def get_twitter_card_info(post)
     twitter_card = {}
     if post.present?
       if post.id.present?
-        twitter_card[:url] = "https://answer-me-716.herokuapp.com/posts/#{post.id}"
+        twitter_card[:url] = "https://answer-me-716.herokuapp.com/answer/#{post.id}"
         twitter_card[:image] = "https://s3-ap-northeast-1.amazonaws.com/answer-me-716-production/images/#{post.id}.png"
       else
         twitter_card[:url] = 'https://answer-me-716.herokuapp.com/'
